3750ad75e8b56af9531c51b6892ea728c4cf9e85
Richard Hallett <whitefire0@Richards-MacBook-Air.local>
2018-07-10 08:07:17 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+/**
+ * Projec spec:
+ * TODO => it should have a place to store numbers
+ * TODO => it should have a place to store operators
+ * TODO => it should have an API that transfers user input to these stores
+ * TODO => it should be able to apply an operator to two numbers
+ * TODO => it should be able to apply an operator to three or more numbers
+ * TODO => it should be able to apply operators in order of presedence
+ * TODO => it should have a memory function
+ * TODO => it should have a clear memory function
+ * TODO => it should keep a history of calculations
+ * TODO => it should have an ES5 and ES6 version
+ * TODO => it should have a full tinytest.js test suite
+ * TODO => it should be able to recall this history of calculations
+ * TODO => it could calculate statistics of overall calculator use
+ * 
+ */
+
+ /**
+  * Understanding required to read this code:
+  * variable assignment
+  * functions, function returns
+  * data structures: arrays, objects
+  * IIFEs
+  * 
+  */
+
